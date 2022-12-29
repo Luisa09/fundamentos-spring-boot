e1c95ba9e0e16ac91801ad9b19686fd523c8f0e4
Luisa09 <luisalds.99@gmail.com>
2022-12-29 15:26:32 -0500

    first commit

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//metodo main: encargado de inicializar la arquitedctura y la autoconfiguracion del proyecto
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
